7859bda2d9cffaee0751055860f3bd2ed9eb05a8
wjonke <wesjonke@gmail.com>
2020-04-20 11:53:43 -0400

    changed url to imgurl in skills table

--- database/migrations/20200415200652_skills.js
+++ database/migrations/20200415200652_skills.js
@@ -4,7 +4,7 @@ exports.up = function (knex) {
   return knex.schema.createTable("skills", (skills) => {
     skills.increments("id").primary();
     skills.string("name", 255).notNullable();
-    skills.string("url", 255).notNullable();
+    skills.string("imgUrl", 255).notNullable();
     skills.timestamps(true, true);
   });
 };
