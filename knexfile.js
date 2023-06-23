69088d2afc8e0db15d1c26d427ab8ece39ed07df
Wes Jonke <wesjonke@gmail.com>
2023-06-23 13:54:19 -0400

    fix backend

--- knexfile.js
+++ knexfile.js
@@ -1,55 +1,55 @@
 module.exports = {
-  development: {
-    client: "sqlite3",
-    useNullAsDefault: true,
-    connection: {
-      filename: "./database/database.db3",
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: "./database/migrations",
-    },
-    seeds: {
-      directory: "./database/seeds",
-    },
-  },
-  ////////////////////////////////////////////////////////
+	development: {
+		client: 'sqlite3',
+		useNullAsDefault: true,
+		connection: {
+			filename: './database/database.db3',
+		},
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		migrations: {
+			directory: './database/migrations',
+		},
+		seeds: {
+			directory: './database/seeds',
+		},
+	},
+	////////////////////////////////////////////////////////
 
-  testing: {
-    client: "sqlite3",
-    connection: {
-      filename: "./database/test.db3",
-    },
-    useNullAsDefault: true,
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      directory: "./database/migrations",
-    },
-    seeds: {
-      directory: "./database/seeds",
-    },
-  },
-  ////////////////////////////////////////////////////////
+	testing: {
+		client: 'pg',
+		connection: {
+			filename: './database/test.db3',
+		},
+		useNullAsDefault: true,
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		migrations: {
+			directory: './database/migrations',
+		},
+		seeds: {
+			directory: './database/seeds',
+		},
+	},
+	////////////////////////////////////////////////////////
 
-  production: {
-    client: "pg",
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    useNullAsDefault: true,
-    migrations: {
-      directory: "./database/migrations",
-    },
-    seeds: {
-      directory: "./database/seeds",
-    },
-  },
+	production: {
+		client: 'pg',
+		connection: process.env.DATABASE_URL,
+		pool: {
+			min: 2,
+			max: 10,
+		},
+		useNullAsDefault: true,
+		migrations: {
+			directory: './database/migrations',
+		},
+		seeds: {
+			directory: './database/seeds',
+		},
+	},
 };
